c86775cba061b600b60812fc8b78ef9091a200eb
TylerJulian <Tyler.james.julian@ou.edu>
2018-03-04 02:02:11 -0600

    upload new changes

---
+++ Assets/com/Cheesesquad/CardboardEducation/scripts/toggleRotation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class toggleRotation : MonoBehaviour {
+    public bool spinQ = false;
+    // Use this for initialization
+    void Start() {
+
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (spinQ == true)
+    {
+
+            transform.Rotate(Vector3.up * 5 * Time.deltaTime);
+
+        }
+
+
+    }
+
+    public void Toggle()
+    {
+        spinQ = !spinQ;
+    }
+    public void enter() {
+        spinQ = false;
+                            }
+    public void exit()
+    {
+        spinQ = true;
+    }
+
+}
